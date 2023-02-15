8f9fe982f97fa8a578f1476a83ef839b3de4e2a0
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-15 17:59:23 +0300

    font-family

--- src/Companents/LessonCard/LessonCard.jsx
+++ src/Companents/LessonCard/LessonCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { data } from '../helper/data'
+import './LessonCard.css'
 const LessonCard = () => {
 
   return (
@@ -8,7 +9,11 @@ const LessonCard = () => {
         {
             data.map(({image,name,age})=>(
              <div className='section'>
-                <img src={image} alt="" />
+                <img width="180px" height="180px" src={image} alt="" />
+                <div className='card'>
+                    <h3> <span>Lesson Name : </span> {name}</h3>
+                    <h3> <span>Lesson Name : </span> {age} </h3>
+                </div>
             </div>
             )) }
         
